e1097b166cf83f43b6635b8f1e773bdc46d2902f
Ethereal-bang <939847757@qq.com>
2022-09-19 18:04:16 +0800

    update: project init

---
+++ src/main/java/online_chat_server/OnlineChatServerApplication.java
@@ -0,0 +1,13 @@
+package online_chat_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineChatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineChatServerApplication.class, args);
+    }
+
+}
