207f74a49acad2e8b6697dd9c00aa910737d9a12
Ethereal-bang <939847757@qq.com>
2022-09-20 21:57:55 +0800

    update: CORS

---
+++ src/main/java/online_chat_server/common/WebAppConfigurer.java
@@ -0,0 +1,18 @@
+package online_chat_server.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebAppConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+    }
+
+}
