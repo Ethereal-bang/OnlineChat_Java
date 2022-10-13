69828a474aa8eec7ff1ec17d755520b91144d744
Ethereal-bang <939847757@qq.com>
2022-10-13 20:31:41 +0800

    fix: 消息时间时区错误

--- src/main/java/online_chat_server/pojo/News.java
+++ src/main/java/online_chat_server/pojo/News.java
@@ -2,6 +2,7 @@ package online_chat_server.pojo;
 
 import lombok.*;
 
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -16,10 +17,10 @@ public class News {
     private int receiver;
     private String content; // markdown
     private String word;    // 纯文本
-    private Date time;
+    private Timestamp time;
 
     public String getTime() {
-        SimpleDateFormat fd = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        SimpleDateFormat fd = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return fd.format(time);
     }
 
